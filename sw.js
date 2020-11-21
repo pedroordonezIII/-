a08619f683b5dfd7f3fd5d2268de67ac8017ee50
pedroordonezIII <pedro.ordoneziii@gmail.com>
2020-11-21 14:54:53 -0600

    Service workers and caching

--- sw.js
+++ sw.js
@@ -39,51 +39,6 @@ self.addEventListener('fetch', function(event) {
 
 
 
-// const filesToCache = [
-// 	'/',
-//     'index.html',
-//     'cities.html',
-//     'forecast.html',
-//     'searchForecast.html',
-//     'src/styles/styles.css', 
-//     'src/js/cityWeather.js', 
-//     'src/js/forecast.js', 
-//     'src/js/recentLocations.js', 
-//     'src/js/searchForecast.js', 
-//     'src/js/searchWeatherApp.js', 
-//     'src/js/weatherApp.js' 
-//   ];
-  
-//   const staticCacheName = 'pages-cache-v1';
-  
-//   self.addEventListener('install', event => {
-//     console.log('Attempting to install service worker and cache static assets');
-//     event.waitUntil(
-//       caches.open(staticCacheName)
-//       .then(cache => {
-//       	console.log("Opened cache")
-//         cache.addAll(filesToCache.map(function(filesToCache) {
-// 	    return new Request(filesToCache, { mode: 'no-cors' });
-// 	    })).then(function() {
-// 	    console.log('All resources have been fetched and cached.');
-// 	    });
-//       })
-//     );
-//   });
-
-//  const staticCacheName = 'pages-cache-v1';
-
-// self.addEventListener('install', event => {
-//   console.log('Attempting to install service worker and cache static assets');
-//   event.waitUntil(
-//     caches.open(staticCacheName)
-//     .then(cache => {
-//       return cache.addAll(filesToCache);
-//     })
-//   );
-// });
-
-
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
 
