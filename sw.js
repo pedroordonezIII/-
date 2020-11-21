cfb9cfc5a27a7b1734bd21a145e8157881b294f7
pedroordonezIII <pedro.ordoneziii@gmail.com>
2020-11-21 06:33:21 -0600

    Service workers and caching

--- sw.js
+++ sw.js
@@ -1,38 +1,11 @@
 
-// self.addEventListener('fetch', function(event) {
-//     event.respondWith(
-//       fetch(event.request).catch(function() {
-//         return caches.match(event.request);
-//       })
-//     );
-//   });
-
-
-var networkDataReceived = false;
-
-startSpinner();
-
-// fetch fresh data
-var networkUpdate = fetch('/data.json').then(function(response) {
-  return response.json();
-}).then(function(data) {
-  networkDataReceived = true;
-  updatePage(data);
-});
-
-// fetch cached data
-caches.match('/data.json').then(function(response) {
-  if (!response) throw Error("No data");
-  return response.json();
-}).then(function(data) {
-  // don't overwrite newer network data
-  if (!networkDataReceived) {
-    updatePage(data);
-  }
-}).catch(function() {
-  // we didn't get cached data, the network is our last hope:
-  return networkUpdate;
-}).catch(showErrorMessage).then(stopSpinner());
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      fetch(event.request).catch(function() {
+        return caches.match(event.request);
+      })
+    );
+  });
 
 
 
