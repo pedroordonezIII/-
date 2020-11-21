464342fed1e81707ecd3c6ea0e9b82644dfa3a48
pedroordonezIII <pedro.ordoneziii@gmail.com>
2020-11-21 08:41:18 -0600

    Service workers and caching

--- sw.js
+++ sw.js
@@ -33,9 +33,9 @@ self.addEventListener('fetch', function(event) {
 //     );
 //   });
 
-// self.addEventListener('fetch', function(event) {
-//     event.respondWith(fetch(event.request));
-//   });
+self.addEventListener('fetch', function(event) {
+    event.respondWith(fetch(event.request));
+  });
 
 
 
