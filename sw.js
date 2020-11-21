36da8298ee1faf73dd3afd60a20db71c1f028c74
pedroordonezIII <pedro.ordoneziii@gmail.com>
2020-11-21 07:19:33 -0600

    Service workers and caching

--- sw.js
+++ sw.js
@@ -1,49 +1,49 @@
 
-// self.addEventListener('fetch', function(event) {
-//     event.respondWith(
-//       fetch(event.request).catch(function() {
-//         return caches.match(event.request);
-//       })
-//     );
-//   });
-
 self.addEventListener('fetch', function(event) {
-    event.respondWith(fetch(event.request));
-  });
-
-
-
-const filesToCache = [
-	'/',
-    'index.html',
-    'cities.html',
-    'forecast.html',
-    'searchForecast.html',
-    'src/styles/styles.css', 
-    'src/js/cityWeather.js', 
-    'src/js/forecast.js', 
-    'src/js/recentLocations.js', 
-    'src/js/searchForecast.js', 
-    'src/js/searchWeatherApp.js', 
-    'src/js/weatherApp.js' 
-  ];
-  
-  const staticCacheName = 'pages-cache-v1';
-  
-  self.addEventListener('install', event => {
-    console.log('Attempting to install service worker and cache static assets');
-    event.waitUntil(
-      caches.open(staticCacheName)
-      .then(cache => {
-      	console.log("Opened cache")
-        cache.addAll(filesToCache.map(function(filesToCache) {
-	    return new Request(filesToCache, { mode: 'no-cors' });
-	    })).then(function() {
-	    console.log('All resources have been fetched and cached.');
-	    });
+    event.respondWith(
+      fetch(event.request).catch(function() {
+        return caches.match(event.request);
       })
     );
   });
+
+// self.addEventListener('fetch', function(event) {
+//     event.respondWith(fetch(event.request));
+//   });
+
+
+
+// const filesToCache = [
+// 	'/',
+//     'index.html',
+//     'cities.html',
+//     'forecast.html',
+//     'searchForecast.html',
+//     'src/styles/styles.css', 
+//     'src/js/cityWeather.js', 
+//     'src/js/forecast.js', 
+//     'src/js/recentLocations.js', 
+//     'src/js/searchForecast.js', 
+//     'src/js/searchWeatherApp.js', 
+//     'src/js/weatherApp.js' 
+//   ];
+  
+//   const staticCacheName = 'pages-cache-v1';
+  
+//   self.addEventListener('install', event => {
+//     console.log('Attempting to install service worker and cache static assets');
+//     event.waitUntil(
+//       caches.open(staticCacheName)
+//       .then(cache => {
+//       	console.log("Opened cache")
+//         cache.addAll(filesToCache.map(function(filesToCache) {
+// 	    return new Request(filesToCache, { mode: 'no-cors' });
+// 	    })).then(function() {
+// 	    console.log('All resources have been fetched and cached.');
+// 	    });
+//       })
+//     );
+//   });
 
 //  const staticCacheName = 'pages-cache-v1';
 
@@ -56,3 +56,27 @@ const filesToCache = [
 //     })
 //   );
 // });
+
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/src/styles/styles.css',
+  '/src/js/weatherApp/js', 
+  '/index.html',
+  '/cities.html',
+  '/forecast.html',
+  '/searchForecast.html',
+
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
