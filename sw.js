ec0e825c87d0643701e34efa5f15424709b475ca
pedroordonezIII <pedro.ordoneziii@gmail.com>
2020-11-21 08:24:33 -0600

    Service workers and caching

--- sw.js
+++ sw.js
@@ -11,26 +11,26 @@
 //       )
 //     );
 //   });
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.open('mysite-dynamic').then(function(cache) {
-        return cache.match(event.request).then(function (response) {
-          return response || fetch(event.request).then(function(response) {
-            cache.put(event.request, response.clone());
-            return response;
-          });
-        });
-      })
-    );
-  });
-
 // self.addEventListener('fetch', function(event) {
 //     event.respondWith(
-//       fetch(event.request).catch(function() {
-//         return caches.match(event.request);
+//       caches.open('mysite-dynamic').then(function(cache) {
+//         return cache.match(event.request).then(function (response) {
+//           return response || fetch(event.request).then(function(response) {
+//             cache.put(event.request, response.clone());
+//             return response;
+//           });
+//         });
 //       })
 //     );
 //   });
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      fetch(event.request).catch(function() {
+        return caches.match(event.request);
+      })
+    );
+  });
 
 // self.addEventListener('fetch', function(event) {
 //     event.respondWith(fetch(event.request));
