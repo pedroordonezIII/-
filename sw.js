eba3aac70b9e5ac84579b8b5f7a869de072dd5da
pedroordonezIII <pedro.ordoneziii@gmail.com>
2020-11-30 03:10:47 -0600

    Comments

--- sw.js
+++ sw.js
@@ -25,13 +25,13 @@ self.addEventListener('fetch', function(event) {
     );
   });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      fetch(event.request).catch(function() {
-        return caches.match(event.request);
-      })
-    );
-  });
+// self.addEventListener('fetch', function(event) {
+//     event.respondWith(
+//       fetch(event.request).catch(function() {
+//         return caches.match(event.request);
+//       })
+//     );
+//   });
 
 // self.addEventListener('fetch', function(event) {
 //     event.respondWith(fetch(event.request));
