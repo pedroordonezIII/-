efcd0847685a9b3f00d1b6490dbb9ebd6856eca9
pedroordonezIII <pedro.ordoneziii@gmail.com>
2020-12-04 19:59:33 -0600

    Update src

--- registerSW.js
+++ registerSW.js
@@ -3,10 +3,10 @@ if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('sw.js').then(function(registration) {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        btnAdd2.addEventListener("click", (e) => {
-        btnAdd2.style.display = "none";
-        registration.update(); 
-      });
+      //   btnAdd2.addEventListener("click", (e) => {
+      //   btnAdd2.style.display = "none";
+      //   registration.update(); 
+      // });
       }, function(err) {
         // registration failed :(
         console.log('ServiceWorker registration failed: ', err);
